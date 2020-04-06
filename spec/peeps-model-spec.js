4ea7418363b418b9a77bbfa93daebe28a8e15dc8
James Oddy <jamesoddy8@gmail.com>
2020-04-06 09:16:36 +0100

    first test refactored and failing

--- spec/peeps-model-spec.js
+++ spec/peeps-model-spec.js
@@ -1,11 +1,18 @@
-describe('PeepsModel', function() {
-  describe('.fetchPeeps', function() {
-    it('fetches peeps data from API', function() {
-      var peepsView = new PeepsView();
-      var peeps = [{"body": "test one"}, {"body": "test two"}];
-      expect(peepsView.wrapInHTML(peeps)).toEqual(
-        "<ul><li><div>test one</div></li><li><div>test two</div></li></ul>"
-      );
-    });
-  });
-});
+"use strict"
+
+(function testPeepModelCanAccessPeep() {
+  var apiCallResponse = [{
+    'id':1,
+    "body": "Test chitter my first peep",
+    "created_at": "Mon  6 Apr 2020 08:44:17 BST",
+    "updated_at": "Mon  6 Apr 2020 08:44:17 BST",
+    "user":{
+      "id":7,
+      "handle":"twitterbot242092"
+      },
+    "likes": [{"user":{"id":7,"handle":"twitterbot242092"}}]
+  }]
+  var peep = new Peep(apiCallResponse[0])
+  assert.isTrue(peep.body() === "Test chitter my first peep")
+  console.log("Test: Peep model can access a headline PASSED")
+})();
